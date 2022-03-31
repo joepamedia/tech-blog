44f3cc2ed34d87ae728174c37671ff8ff30d19f6
joepamedia <josephpalmisano18@gmail.com>
2022-03-31 14:57:58 -0500

    adjusted variable names & made Comment Model

---
+++ seeds/seed.js
@@ -0,0 +1,41 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+  // making users
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  // add user id to each post
+  const formattedPosts = () => {
+    const updatedPost = [];
+    postData.forEach((element) => {
+      element.user_id = users[Math.floor(Math.random() * users.length)].id;
+      updatedPost.push(element);
+    });
+    return updatedPost;
+  };
+  const updatedPosts = formattedPosts();
+  //  making posts
+  const posts = await Post.bulkCreate(updatedPosts, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      post_id: posts[Math.floor(Math.random() * posts.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
